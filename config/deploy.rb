c06332c9107501060480c1f9be94d6548de758c0
Alagu <allagappan@gmail.com>
2013-03-11 23:07:09 +0530

    Put back all 3 ips

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,7 @@ set :repository,  "git@github.com:alagu/pnrapi-ruby.git"
 set :user, "alagu"
 set :deploy_to, "/home/alagu/pnrapi"
 
-# role :web, "198.101.212.213", "198.101.212.248", "198.101.212.15"
-role :web, "198.101.212.213"
+role :web, "198.101.212.213", "198.101.212.248", "198.101.212.15"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
